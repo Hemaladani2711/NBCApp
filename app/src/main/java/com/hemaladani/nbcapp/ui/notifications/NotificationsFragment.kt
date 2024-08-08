1e8f29c9c9687ab645c47e445dea5f78e369ff84
Hemal Adani <hkdeveloper2711@gmail.com>
2024-08-08 15:55:20 -0400

    removing notifications.xml and converting to compose

--- app/src/main/java/com/hemaladani/nbcapp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/hemaladani/nbcapp/ui/notifications/NotificationsFragment.kt
@@ -4,36 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.hemaladani.nbcapp.R
-import com.hemaladani.nbcapp.databinding.FragmentNotificationsBinding
-import com.hemaladani.nbcapp.ui.dashboard.helloWorld_Text
 
 class NotificationsFragment : Fragment() {
-
-    private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        val textNotifications = root.findViewById<ComposeView>(R.id.text_notifications)
-        textNotifications.setContent { helloWorld_Text() }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return ComposeView(requireContext()).apply {
+            setContent { notifications_text() }
+        }
     }
 }
---
+++ app/src/main/java/com/hemaladani/nbcapp/ui/notifications/NotificationsScreen.kt
@@ -0,0 +1,11 @@
+package com.hemaladani.nbcapp.ui.notifications
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
+
+    //@Preview
+    @Composable
+    fun notifications_text(){
+        Text("This is Notifications Fragment", color = Color.White)
+    }
